4846a40393c12cf7dd3544dde6920460a791816f
LEE-NS <ogawa9341@gmail.com>
2023-11-27 23:47:05 +0900

    script start & header layout need to be update

--- js/script01.js
+++ js/script01.js
@@ -10,4 +10,14 @@ header
     열릴 때, gnb의 항목 이외에는 상호작용 할 수 없도록 한다. 스크롤도 X. (모바일 한정)
     열릴 때, gnb에 가려진 항목들은 어둡게 음영처리한다
  */
-let Gnb = document.querySelector('.gnb');
+let gnbWindow = document.querySelector('.gnb_wrap');
+let gnbExit = document.querySelector('.gnb_exit');
+let gnbBtn = document.querySelector('.gnb_btn');
+
+
+gnbExit.addEventListener('click', function() {
+    gnbWindow.style.display = 'none';
+}); //GNB창 닫기
+gnbBtn.addEventListener('click', function() {
+    gnbWindow.style.display = 'flex';
+});
