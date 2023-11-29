cfc91c3f2dbaa43e76bc3041c0a8c0dc2101b5a5
LEE-NS <ogawa9341@gmail.com>
2023-11-29 19:32:08 +0900

    header script

--- js/script01.js
+++ js/script01.js
@@ -2,22 +2,32 @@
 header
 
 - gnb에서 X표시 누르면 gnb 창이 닫힘
-    빠른 전환 효과 (0.3s 미만)
-    전환 효과는 '단순 사라짐 효과'
+    yes 전환 효과는 '단순 사라짐 효과'
 
 - 햄버거 표시를 누르면 gnb 창이 열림 
-    빠른 전환 효과 (0.3s 미만, 창이 닫히는 빠르기와 동일하게)
-    열릴 때, gnb의 항목 이외에는 상호작용 할 수 없도록 한다. 스크롤도 X. (모바일 한정)
-    열릴 때, gnb에 가려진 항목들은 어둡게 음영처리한다
+    열릴 때, gnb의 항목 이외에는 상호작용 할 수 없도록 한다. 스크롤도 X.
+    yes 열릴 때, gnb에 가려진 항목들은 어둡게 음영처리한다
  */
+
 let gnbWindow = document.querySelector('.gnb_wrap');
+let gnbList = document.querySelector('.gnb');
 let gnbExit = document.querySelector('.gnb_exit');
 let gnbBtn = document.querySelector('.gnb_btn');
 
+function ctrlGnbWindow() {
+    let dp = gnbWindow.style.display;
 
-gnbExit.addEventListener('click', function() {
-    gnbWindow.style.display = 'none';
-}); //GNB창 닫기
-gnbBtn.addEventListener('click', function() {
-    gnbWindow.style.display = 'flex';
-});
+    if (dp == 'flex') {
+        gnbWindow.style.backgroundColor = 'transparent';
+        gnbList.style.backgroundColor = 'transparent';
+        gnbWindow.style.display = 'none'
+    } else {
+        gnbWindow.style.backgroundColor = '#11111199';
+        gnbList.style.backgroundColor = '#ffffff';
+        gnbWindow.style.display = 'flex'
+    };
+}; 
+//GNB 열기 및 닫기
+
+gnbExit.addEventListener('click', ctrlGnbWindow);
+gnbBtn.addEventListener('click', ctrlGnbWindow);
