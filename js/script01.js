718c1145978f7e7b03d696f71ba0638901b3a273
LEE-NS <ogawa9341@gmail.com>
2023-12-01 20:04:34 +0900

    gnb mark up

--- js/script01.js
+++ js/script01.js
@@ -7,12 +7,22 @@ header
 - 햄버거 표시를 누르면 gnb 창이 열림 
     열릴 때, gnb의 항목 이외에는 상호작용 할 수 없도록 한다. 스크롤도 X.
     yes 열릴 때, gnb에 가려진 항목들은 어둡게 음영처리한다
+
+- 아코디언 메뉴
+    각 탭을 클릭할 시 그 탭의 하위 항목들이 나타난다
+    하위 항목들이 나타나면서 탭의 왼쪽 인디케이터가 빨간색으로 변하고 오른쪽의 메뉴 확장 여부를 나타내는 기호가 '-'로 변한다
+    각 탭을 다시 클릭하면 그 탭의 하위 항목들이 사라진다
+    하위 항목들이 사라지면서 탭의 왼쪽 인디케이터가 사라지고 오른쪽의 메뉴 확장 여부를 나타내는 기호가 '+'로 변한다
+    (gnb_menu_d0)
+
  */
 
 let gnbWindow = document.querySelector('.gnb_wrap');
 let gnbList = document.querySelector('.gnb');
 let gnbExit = document.querySelector('.gnb_exit');
 let gnbBtn = document.querySelector('.gnb_btn');
+
+
 
 function ctrlGnbWindow() {
     let dp = gnbWindow.style.display;
@@ -29,5 +39,9 @@ function ctrlGnbWindow() {
 }; 
 //GNB 열기 및 닫기
 
+
+
+
 gnbExit.addEventListener('click', ctrlGnbWindow);
 gnbBtn.addEventListener('click', ctrlGnbWindow);
+//해당 태그를 클릭할 시 GNB를 열고 닫기
