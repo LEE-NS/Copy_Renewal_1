8fcb58a4a9062ee7ffd7662b1c63b14a42c594a1
LEE-NS <ogawa9341@gmail.com>
2023-12-06 17:16:43 +0900

    accordian menu fail

--- js/script01.js
+++ js/script01.js
@@ -8,24 +8,24 @@ header
     열릴 때, gnb의 항목 이외에는 상호작용 할 수 없도록 한다. 스크롤도 X.
     yes 열릴 때, gnb에 가려진 항목들은 어둡게 음영처리한다
 
-- 아코디언 메뉴
-    각 탭을 클릭할 시 그 탭의 하위 항목들이 나타난다
-    하위 항목들이 나타나면서 탭의 왼쪽 인디케이터가 빨간색으로 변하고 오른쪽의 메뉴 확장 여부를 나타내는 기호가 '-'로 변한다
-    각 탭을 다시 클릭하면 그 탭의 하위 항목들이 사라진다
-    하위 항목들이 사라지면서 탭의 왼쪽 인디케이터가 사라지고 오른쪽의 메뉴 확장 여부를 나타내는 기호가 '+'로 변한다
-    (gnb_menu_d0)
+- 아코디언 메뉴 : 
+    핵심은 클릭 이벤트에 대한 처리로, 해당 섹션을 활성화하고 다른 섹션들을 비활성화하는 로직을 구현하는 것이 중요합니다.
+    
+    활성화된 아코디언 메뉴의 스타일이 저장된 HTML 클래스(.active)를 작성해둔다.
+    어떤 항목을 클릭하면 .active가 그 항목에 적용된다
+    어떤 항목이 클릭되어 활성화된 상태에서 다른 항목을 클릭하면 .active가 동일한 모든 항목에서 제거되고 마지막에 클릭한 항목에 .active가 적용된다.
 
+    jquery에서는 어떤 로직을 사용했을까?
  */
 
 let gnbWindow = document.querySelector('.gnb_wrap');
 let gnbList = document.querySelector('.gnb');
 let gnbExit = document.querySelector('.gnb_exit');
 let gnbBtn = document.querySelector('.gnb_btn');
-let menuD0 = document.querySelector('.gnb_menu_d0_title');
-let menuD1 = document.querySelector('.gnb_menu_d1');
-let openIndic = document.querySelector('.open_indic');
-let plusSymbol = document.getElementsByClassName('plus_symbol');
-let ctrlPlusSymbol = document.querySelectorAll('.plus_symbol span')[1];
+let menuD0 = [...document.querySelectorAll('.gnb_menu_d0')];
+let menuD1 = [...document.querySelectorAll('.gnb_menu_d1')];
+let menuD2 = [...document.querySelectorAll('.gnb_menu_d2')];
+let menuD3 = [...document.querySelectorAll('.gnb_menu_d3')];
 
 function ctrlGnbWindow() {
     let dp = gnbWindow.style.display;
@@ -40,33 +40,25 @@ function ctrlGnbWindow() {
         gnbWindow.style.display = 'flex'
     };
 }; 
-//GNB 창 열기 및 닫기
+//GNB 창 열기(이거보다 훨씬 깔끔하게 할 수 있을듯. 바꾸기)
 
-function ctrlGnbAccrd(topMenu ,bottomMenu) {
-    let accrd = bottomMenu.style.height;
-
-    if (accrd == '0px') {
-        bottomMenu.style.height = '100%';
-        topMenu.style.backgroundColor = '#eee';
-        openIndic.style.backgroundColor = '#ff0000';
-        ctrlPlusSymbol.style.transform = 'rotate(0deg)';
-        //열기
-    } else {
-        bottomMenu.style.height = '0';
-        topMenu.style.backgroundColor = '#fff';
-        openIndic.style.backgroundColor = '#fff';
-        ctrlPlusSymbol.style.transform = 'rotate(90deg)';
-        //닫기
+function openAccrd(bottoms) {
+    bottoms.forEach(elem => {
+        let bottom = elem;
+    });
+    for (let i = 0; i < bottoms; i++) {
+        bottoms[i].classList.remove('active_menu')
     };
+    bottom.classList.add('active_menu');
 };
-//아코디언 메뉴 열기 및 닫기
+//아코디언 형태 메뉴
 
 
-gnbExit.addEventListener('click', () => (ctrlGnbWindow()));
-gnbBtn.addEventListener('click', () => (ctrlGnbWindow()));
+gnbExit.addEventListener('click', ctrlGnbWindow);
+gnbBtn.addEventListener('click', ctrlGnbWindow);
 //해당 태그를 클릭할 시 GNB를 열고 닫기
 
-menuD0.addEventListener('click', function() {
-    ctrlGnbAccrd(menuD0 ,menuD1)
-});
-/* 콜백에 대한 부분을 읽어볼 것 */
+menuD0.addEventListener('click', () => (openAccrd(menuD1)));
+menuD1.addEventListener('click', () => (openAccrd(menuD2)));
+menuD2.addEventListener('click', () => (openAccrd(menuD3)));
+//해당 태그를 클릭 시 아코디언 형태 메뉴를 열고 닫기
